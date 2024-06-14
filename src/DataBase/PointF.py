2e04f3bb5ddab37e81e0e85a2ca25290f8c99c8a
Genidiot <fynn_cheng@outlook.com>
2024-06-14 18:44:19 +0800

    change

--- src/DataBase/PointF.py
+++ src/DataBase/PointF.py
@@ -11,11 +11,11 @@ class PointF:
     def set_column(self, column):
         self.x = column
 
-    def set_row(self, row):
-        self.y = row
-
     def get_column(self):
         return self.x
+
+    def set_row(self, row):
+        self.y = row
 
     def get_row(self):
         return self.y
